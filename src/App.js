01d17c419ce888bc69143178da94b346351605d1
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-12 14:52:17 -0500

    created class component for App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+const { Component } = require("react");
+
+class App extends Component{
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     count: 0,
+  //   }
+  // }
+
+  increment=()=>{
+    
+  }
+
+  render(){
+    let count = 0;
+    return(
+      <div>Happy Hacking</div>
+    )
+  }
+}
